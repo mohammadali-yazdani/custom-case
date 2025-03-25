7816863eb53a23fa331da94c7b0706cd500dbc94
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-03-25 20:06:09 +0300

    feat: add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,99 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
+  return (
+    <nav className="sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="z-40 flex font-semibold">
+            custom<span className="text-lime-600">case</span>
+          </Link>
+
+          <div className="flex h-full items-center space-x-4">
+            {user ? (
+              <>
+                <Link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign out
+                </Link>
+                {isAdmin ? (
+                  <Link
+                    href="/api/auth/logout"
+                    className={buttonVariants({
+                      size: "sm",
+                      variant: "ghost",
+                    })}
+                  >
+                    Dashboard ✨
+                  </Link>
+                ) : null}
+
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden gap-1 sm:flex sm:items-center",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="me-1.5 size-5" />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/api/auth/register"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign up
+                </Link>
+
+                <Link
+                  href="/api/auth/login"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Login
+                </Link>
+
+                <div className="hidden h-8 w-px bg-zinc-200 sm:block" />
+
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "hidden gap-1 sm:flex sm:items-center",
+                  })}
+                >
+                  Create case
+                  <ArrowRight className="ms-1.5 size-5" />
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
