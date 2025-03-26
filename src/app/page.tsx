8643a266f78cdcfcbb59398253c902eca51c9d8f
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-03-26 12:07:12 +0300

    feat: add reviews section to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import Reviews from "@/components/Reviews";
+import { Icons } from "@/components/Icons";
 import { Check, Star } from "lucide-react";
 
 export default function Home() {
@@ -101,6 +103,110 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col items-center gap-4 sm:gap-6 lg:flex-row">
+            <h2 className="order-1 mt-2 text-5xl font-bold tracking-tight text-gray-950 md:text-6xl">
+              What our{" "}
+              <span className="relative px-2">
+                customers
+                <Icons.underline className="pointer-events-none absolute inset-x-0 -bottom-6 hidden text-lime-500 sm:block" />
+              </span>{" "}
+              say
+            </h2>
+
+            <img
+              src="/snake-2.png"
+              className="order-0 w-24 lg:order-2"
+              alt=""
+            />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            {/* user review */}
+            <div className="flex flex-auto flex-col gap-4 lg:ps-8 xl:ps-20">
+              <div className="mb-2 flex gap-0.5">
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "The case feels durable and I even got a compliment on the
+                  design. Had the case for two and a half months now and{" "}
+                  <span className="bg-slate-800 p-0.5 text-white">
+                    the image is super clear
+                  </span>
+                  , on the case I had before, the image started fading into
+                  yellow-ish color after a couple weeks. Love it."
+                </p>
+              </div>
+
+              <div className="mt-2 flex gap-4">
+                <img
+                  className="size-12 rounded-full object-cover"
+                  src="/users/user-1.png"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex items-center gap-1.5 text-zinc-600">
+                    <Check className="size-4 stroke-3 text-lime-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* user review */}
+            <div className="flex flex-auto flex-col gap-4 lg:ps-8 xl:ps-20">
+              <div className="mb-2 flex gap-0.5">
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+                <Star className="size-5 fill-lime-600 text-lime-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "Not gonna lie, this case feels super sturdy, and I’ve already
+                  gotten a few compliments on the design. It’s been almost three
+                  months now, and{" "}
+                  <span className="bg-slate-800 p-0.5 text-white">
+                    the print quality is still flawless
+                  </span>
+                  . My last case? The image started peeling and turned yellow
+                  after just a few weeks. This one’s a total game-changer.
+                  Absolutely love it!"
+                </p>
+              </div>
+
+              <div className="mt-2 flex gap-4">
+                <img
+                  className="size-12 rounded-full object-cover"
+                  src="/users/user-4.jpg"
+                  alt="user"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Morad</p>
+                  <div className="flex items-center gap-1.5 text-zinc-600">
+                    <Check className="size-4 stroke-3 text-lime-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
