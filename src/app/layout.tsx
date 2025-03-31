19fbeae3f11fd58055cac98558be8ecfbc51cc26
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-03-31 11:13:14 +0300

    feat: insert Toaster component in layout and fix main class names order

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +31,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
-        <Footer />
+        <main className="flex min-h-[calc(100vh-3.5rem-1px)] flex-col">
+          <div className="flex h-full flex-1 flex-col">{children}</div>
+          <Footer />
+        </main>
+
+        <Toaster />
       </body>
     </html>
   );
