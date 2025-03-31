ab8effaa308d3df6588f3c8d52afac1e2bc0e26e
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2025-03-31 11:12:35 +0300

    feat: configure uploadthing package

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,31 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { z } from "zod";
+
+const f = createUploadthing();
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  imageUploader: f({
+    image: {
+      /**
+       * For full list of options and defaults, see the File Route API reference
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .input(z.object({ configId: z.string().optional() }))
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ input }) => {
+      return { input };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const { configId } = metadata.input;
+      return { configId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
